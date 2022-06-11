0b1151f56f77f93bb282cd82a15803334439b87d
Ryan Mottola <rmottola68@gmail.com>
2022-06-10 22:47:52 -0400

    commit tree 973d1d6ff56c16c50a998b2bf40317df3f3b97c5

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = 0;
+
+let batteries = (oldNum, newNum) => {
+    return oldNum += newNum;
+
+}
+
+totalBatteries = batteryBatches.reduce(batteries);
+
+
+
+
+console.log(totalBatteries);
